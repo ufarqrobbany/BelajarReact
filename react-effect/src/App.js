3979ddd07f2c9a0becc9410a4ea410d0ed9f156a
Umar Faruq Robbany <faruqrobbany@gmail.com>
2022-12-08 15:21:20 +0700

    update

---
+++ react-effect/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Timers from './pages/Timers';
+import Counter from './pages/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Timers />
+      <hr />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-effect/src/pages/Timers.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+function Timers() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count + 1);
+        }, 1000);
+    });
+
+    return <h1>I've rendered {count} times!</h1>;
+}
+
+export default Timers;
